3b0c8f25154bf581d1f2b2bc678baa8da65d9d70
Yoga Harisistianugroho <yogahariss@gmail.com>
2023-11-05 20:31:15 +0700

    dart library

---
+++ example/test.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello('Yoga'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
